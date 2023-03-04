57a8eedeb10f6e5426ab9ed322031c3f77a84bbb
GlodBlock <1356392126@qq.com>
2023-03-04 17:34:56 +0800

    bump version

--- src/main/java/glodblock/com/github/orevisualdetector/Main.java
+++ src/main/java/glodblock/com/github/orevisualdetector/Main.java
@@ -23,7 +23,7 @@ import org.apache.logging.log4j.LogManager;
 public class Main {
     public static final String MODID="orevisualdetector";
     public static final String NAME="Ore Visual Detector";
-    public static final String VERSION="1.0.1";
+    public static final String VERSION="2.0.0";
     @SidedProxy(clientSide = "glodblock.com.github.common.ClientProxy", serverSide = "glodblock.com.github.common.CommonProxy")
     public static CommonProxy proxy;
     @Instance(Main.MODID)
